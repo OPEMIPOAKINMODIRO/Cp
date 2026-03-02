5e8fe1a801508f619dd7e000e5bf1f089c7ed83b
OPEMIPOAKINMODIRO <134601118+OPEMIPOAKINMODIRO@users.noreply.github.com>
2026-03-02 14:47:54 +0100

    Refactor server to use dynamic port configuration

--- server.js
+++ server.js
@@ -103,4 +103,8 @@ app.get("/download/:filename", (req, res) => {
   res.download("./public/uploads/" + req.params.filename);
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
